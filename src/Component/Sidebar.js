61b70ab4bdc141a6329a8a753257f3e4c8612ec1
Madhurendra1311 <kmadhu1311@gmail.com>
2021-01-23 18:40:59 +0530

    component created

---
+++ src/Component/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Sidebar.css'
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* Twitter icon */}
+
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+            {/* SidebarOption */}
+        </div>
+    )
+}
