ef5567a6150dfb42a8467a96dddf151a36bae1cc
Madhurendra1311 <kmadhu1311@gmail.com>
2021-01-30 15:19:27 +0530

    widgets part added

--- src/Component/Widgets.js
+++ src/Component/Widgets.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Widgets.css";
+import {TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed} from "react-twitter-embed";
 import SearchIcon from "@material-ui/icons/Search";
 
 export default function Widgets() {
@@ -11,6 +12,17 @@ export default function Widgets() {
             </div>
             <div className="widgets__widgetContainer">
                 <h2>what's happening</h2>
+
+                <TwitterTweetEmbed tweetId={"858551177860055040"} />
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="cleverqazi"
+                    options={{ height: 400 }}
+                />
+                <TwitterShareButton
+                    url={"https://facebook.com/cleverprogrammer"}
+                    options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+                />
             </div>
         </div>
     )
