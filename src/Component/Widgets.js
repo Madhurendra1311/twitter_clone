93e91d03da5183b35b2b2c07bc002accefc54293
Madhurendra1311 <kmadhu1311@gmail.com>
2021-01-30 14:58:30 +0530

    widgets_part

--- src/Component/Widgets.js
+++ src/Component/Widgets.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./Widgets.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 export default function Widgets() {
     return (
         <div className='widgets'>
-            <h2>Widgets</h2>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+            <div className="widgets__widgetContainer">
+                <h2>what's happening</h2>
+            </div>
         </div>
     )
 }
